8c3a3e5d1818227498dc9a702b0c0e8f94d8a341
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-01-16 15:08:59 -0500

    Fixed amortization schedule

--- src/containers/amortizationSchedule/components/monthInfoRow/MonthInfoRow.jsx
+++ src/containers/amortizationSchedule/components/monthInfoRow/MonthInfoRow.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 import './monthInfoRow.css'
 
 const MonthInfoRow = ({ monthInfo }) => {
-    return (
-        <tr className={`amortization-schedule-month-info-row ${monthInfo.monthNumber % 2 === 0 ? "row-light" : "row-dark"}`}>
-            <td className='amortization-schedule-month-info-text'>{monthInfo.monthNumber}</td>
-            <td className='amortization-schedule-month-info-text'>{monthInfo.totalPayment}</td>
-            <td className='amortization-schedule-month-info-text'>{monthInfo.principalPayment}</td>
-            <td className='amortization-schedule-month-info-text'>{monthInfo.interestPayment}</td>
-            <td className='amortization-schedule-month-info-text'>{monthInfo.totalPrincipalPaid}</td>
-            <td className='amortization-schedule-month-info-text'>{monthInfo.newPrincipalBalance}</td>
-        </tr>
-    )
+    if (monthInfo && monthInfo.newPrincipalBalance >= 0) {
+        return (
+            <tr className={`amortization-schedule-month-info-row ${monthInfo.monthNumber % 2 === 0 ? "row-light" : "row-dark"}`}>
+                <td className='amortization-schedule-month-info-text'>{monthInfo.monthNumber + 1}</td>
+                <td className='amortization-schedule-month-info-text'>{monthInfo.totalPayment}</td>
+                <td className='amortization-schedule-month-info-text'>{monthInfo.principalPayment}</td>
+                <td className='amortization-schedule-month-info-text'>{monthInfo.interestPayment}</td>
+                <td className='amortization-schedule-month-info-text'>{monthInfo.totalPrincipalPaid}</td>
+                <td className='amortization-schedule-month-info-text'>{monthInfo.newPrincipalBalance}</td>
+            </tr>
+        )
+    }
 }
 
 export default MonthInfoRow
